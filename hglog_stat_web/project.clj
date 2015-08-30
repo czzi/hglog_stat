2b3fda7f97f705dadee3de2b44ef66b3e3e5f091
r-zzi <greatczzi@gmail.com>
2015-08-30 10:24:45 -0700

    return json

--- hglog_stat_web/project.clj
+++ hglog_stat_web/project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
-                 [hglog_stat "0.1.0-SNAPSHOT"]]
+                 [hglog_stat "0.1.0-SNAPSHOT"]
+                 [cheshire "5.5.0"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler hglog-stat-web.handler/app}
   :profiles
--- hglog_stat_web/src/hglog_stat_web/handler.clj
+++ hglog_stat_web/src/hglog_stat_web/handler.clj
@@ -1,10 +1,20 @@
 (ns hglog-stat-web.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [hglog-stat.core :refer :all]
+            [cheshire.core :refer :all]))
+
+;(defn -main [ & args]
+;  (let [data (analyze-file  "/home/roman/hglog2.txt")
+;        grouping (group-by-info-fields data '(:branch))]
+;    (clojure.pprint/pprint grouping)))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (let [data (analyze-file  "/home/roman/hglog2.txt")
+                    groups (group-by-info-fields data '(:branch))
+                    sorted-groups (sort-by #(:lines-difference (:info %)) groups)]
+                (generate-string sorted-groups {:pretty true})))
   (route/not-found "Not Found"))
 
 (def app
